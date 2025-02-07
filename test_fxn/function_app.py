15478453d67c82b821f0af0b576c9828594537df
delalamo <diego.delalamo@gmail.com>
2025-02-07 06:18:03 +0100

    Addint SMTPLIB email sending

--- test_fxn/function_app.py
+++ test_fxn/function_app.py
@@ -1,7 +1,11 @@
+import os
 import azure.functions as func
 import datetime
 import json
 import logging
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 app = func.FunctionApp()
 
@@ -15,6 +19,18 @@ def main(mytimer: func.TimerRequest) -> None:
 
     if mytimer.past_due:
         logging.info("The timer is past due!")
+
+    message = MIMEMultipart()
+    message["From"] = os.environ.get("MY_EMAIL")
+    message["To"] = os.environ.get("MY_EMAIL")
+    message["Subject"] = f"Papers {datetime.now()}"
+    message.attach(MIMEText("TEST TEST", "plain"))
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        server.login(os.environ.get("MY_EMAIL"), os.environ.get("MY_PW"))
+        server.sendmail(
+            os.environ.get("MY_EMAIL"), os.environ.get("MY_EMAIL"), message.as_string()
+        )
 
     # Your script logic here
     logging.info("Daily script executed successfully.")
